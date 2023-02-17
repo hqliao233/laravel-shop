319cd8819a96a75bd65e493dda0d140d38bb9498
hqliao233 <1915443708@qq.com>
2023-02-17 13:05:39 +0800

    新增test脚本

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'test';
+
+    public function handle()
+    {
+        
+    }
+}
